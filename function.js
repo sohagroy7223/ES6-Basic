15bd2125752b635cc6323ba067798629f9354cd3
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-26 20:46:41 +0600

    if you don't call argument it is don't show nan uor undifint

--- function.js
+++ function.js
@@ -1,6 +1,13 @@
-function sum(num1 = 1, num2 = 1) {
-  const result = num1 + num2;
-  return result;
+// function sum(num1 = 0, num2 = 0) {
+//   const result = num1 + num2;
+//   return result;
+// }
+// const output = sum(20);
+// console.log(output);
+
+function multiply(n1 = 1, n2 = 1) {
+  const multi = n1 * n2;
+  return multi;
 }
-const output = sum(20);
-console.log(output);
+const result = multiply(8);
+console.log(result);
