19dac8926649199b575cf4d76d61baab3b4fe5af
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-27 09:01:42 +0600

    addad arrow function and templete function

---
+++ arrowFunction.js
@@ -0,0 +1,18 @@
+// //normal function
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+// const results = sum(10, 30);
+// console.log(results);
+
+// // arrowFunction with return call**
+
+// const multi = (p1, p2) => {
+//   return p1 * p2;
+// };
+// const output = multi(5, 7);
+// console.log(output);
+
+const jog = (n1, n2) => n1 + n2;
+console.log(jog(20, 10));
---
+++ templeteString.js
@@ -0,0 +1,10 @@
+// normal string***
+
+const countryName = "Bangladesh";
+
+const anthem = "amar somar " + countryName + " ami tomay valobashi";
+console.log(anthem);
+
+// templete String******
+
+const songit = `amar sonar ${countryName} ami tomay valobashi`;
